29e67b4e4db99fb7028c4f67e11de438c2c3dded
jbrolin <brolin.jonas@gmail.com>
2020-11-12 15:19:46 +0100

    Added structure Added initial styling Added ingredients

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Aux from '../../hox/Auxiliary';
+import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.Module.css';
 
 const layout = ( props ) => (
     <Aux>
-            <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>
         {props.children}
     </main>
     </Aux>
