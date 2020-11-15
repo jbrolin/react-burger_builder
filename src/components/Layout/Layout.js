f740fe34877789959361a7d4780d0d67f1a23361
jbrolin <brolin.jonas@gmail.com>
2020-11-15 17:06:13 +0100

    added backdrop to sidemenu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.Module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer />
-    <main className={classes.Content}>
-        {props.children}
-    </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerCloserHander = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render () {
+        return (
+            <Aux>
+            <Toolbar />
+            <SideDrawer 
+                open={this.state.showSideDrawer} 
+                closed={this.sideDrawerCloserHander} 
+            />
+        <main className={classes.Content}>
+            {this.props.children}
+        </main>
+        </Aux>
+    
+        );
+    
+    }
+}
+
+
+export default Layout;
