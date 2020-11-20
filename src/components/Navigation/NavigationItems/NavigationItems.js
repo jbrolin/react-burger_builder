c00588f9582a2140673a7f4c99578ab2959ea02d
jbrolin <brolin.jonas@gmail.com>
2020-11-20 17:02:42 +0100

    adding form for auth login

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,6 +6,7 @@ const navigationItems = (props) => (
 <ul className={classes.NavigationItems}>
     <NavigationItem link="/">Burger builder</NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/auth">Login</NavigationItem>
 
 </ul>
 );
